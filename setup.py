409c27784814e45dfeb92100ab7f8002bcff2e44
Deepanshu <131351308+deepanshu414@users.noreply.github.com>
2024-08-23 14:56:12 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(name="Playlist_downloader",
+      version="0.0.1",
+      description="Playlist Downloader simplifies the process of downloading YouTube playlists by providing a straightforward API to fetch and save videos from a given playlist URL.",
+      author="Deepanshu antil",
+      maintainer="Deepanshu antil",
+      packages=['Playlist_downloader'],
+      author_email="deepanshuantil4113@gmail.com",
+      url="https://github.com/deepanshu414/playlist_downloader",
+      license="MIT", 
+      install_requires=['pytube'])
